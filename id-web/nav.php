cd1c58bc6c3c227f174a1720c9bb806873419e1c
Carla <carla.arrowsmith@cvp.com>
2022-10-02 14:56:30 +0100

    added i-D

---
+++ id-web/nav.php
@@ -0,0 +1,32 @@
+<div class="uk-position-top">
+    <nav class="uk-navbar-container uk-navbar-transparent uk-margin-large-left uk-margin-large-right uk-margin-top mob-hide" uk-navbar>
+        <div class="uk-navbar-left ">
+            <img src="id-img/i-D.png">
+        </div>
+        <div class="uk-navbar-right">
+            <ul class="uk-navbar-nav" >
+                <li><a style="color: #fff;" href="#">About</a></li>
+                <li><a style="color: #fff;" href="#">My Work</a></li>
+                <li><a style="color: #fff;" href="#">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- mobile nav -->
+    <div class="desktop-hide ">
+		<img class="uk-margin-top uk-margin-left" src="id-img/i-D.png">
+		<a class="uk-margin-top uk-margin-right uk-align-right" href="#offcanvas-usage" uk-icon="icon: menu; ratio: 2" uk-toggle></a>
+		<div id="offcanvas-usage" uk-offcanvas="flip: true">
+		    <div class="uk-offcanvas-bar">
+		        <button class="uk-offcanvas-close" type="button" uk-close></button>
+		        <ul style="list-style: none;">
+                    <li><a style="color: #fff;" href="#">About</a></li>
+                    <li><a style="color: #fff;" href="#">My Work</a></li>
+                    <li><a style="color: #fff;" href="#">Contact</a></li>
+                </ul>
+		    </div>
+		</div>
+    </div>
+</div>
+<div class="hero-text" >
+	<img src="id-img/idtext.png">
+</div>
